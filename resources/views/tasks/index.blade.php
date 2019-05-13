02ab6f9fcbd50380891e71e412238022150c8847
tomoya.fushiki <forrussiaworldcup2018@gmail.com>
2019-05-13 13:39:43 +0000

    kadai-tasklist

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,14 +2,15 @@
 
 @section('content')
 
-    <h1>メッセージ一覧</h1>
+    <h1>タスク一覧</h1>
 
     @if (count($tasks) > 0)
         <table class="table table-striped">
             <thead>
                 <tr>
                     <th>id</th>
-                    <th>メッセージ</th>
+                    <th>タスク</th>
+                    <th>ステータス</th>
                 </tr>
             </thead>
             <tbody>
@@ -17,12 +18,13 @@
                 <tr>
                      <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
                     <td>{{ $task->content }}</td>
+                    <td>{{ $task->status }}</td>
                 </tr>
                 @endforeach
             </tbody>
         </table>
     @endif
     
-    {!! link_to_route('tasks.create', '新規メッセージの投稿', null, ['class' => 'btn btn-primary']) !!}
+    {!! link_to_route('tasks.create', 'タスクの投稿', null, ['class' => 'btn btn-primary']) !!}
 
 @endsection
