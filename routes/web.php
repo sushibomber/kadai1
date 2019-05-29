dfba97251fc33eb73b2fb6b051f72cdf0047df9a
tomoya.fushiki <forrussiaworldcup2018@gmail.com>
2019-05-29 13:50:20 +0000

    user pages

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,8 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 // ユーザ登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+// ユーザ機能
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+});
